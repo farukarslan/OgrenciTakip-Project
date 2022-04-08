dd9e17e5dc9ef99e008f04a536288d7a72834a31
Ömer Faruk Arslan <omerfarukarslan123@hotmail.com>
2022-04-08 13:56:52 +0300

    Form validation işlemleri için gerekli olan attribute ler oluşturuldu.

---
+++ EntityLayer.Model/Attributes/Kod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EntityLayer.Model.Attributes
+{
+    public class Kod : Attribute
+    {
+        public string Description { get; }
+        public string ControlName { get; }
+
+        /// <summary>
+        /// Validation işlemleri sırasında zorunlu olan alanları işaretlemek için kullanılacak
+        /// </summary>
+        /// <param name="description"> Uyarı mesajında gösterilecek olan açıklama </param>
+        /// <param name="controlName"> Uyarı mesajı sonrası focuslanılacak control adı </param>
+        public Kod(string description, string controlName)
+        {
+            Description = description;
+            ControlName = controlName;
+        }
+    }
+}
---
+++ EntityLayer.Model/Attributes/ZorunluAlan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EntityLayer.Model.Attributes
+{
+    public class ZorunluAlan : Attribute
+    {
+        public string Description { get; }
+        public string ControlName { get; }
+
+        /// <summary>
+        /// Validation işlemleri sırasında zorunlu olan alanları işaretlemek için kullanılacak
+        /// </summary>
+        /// <param name="description"> Uyarı mesajında gösterilecek olan açıklama </param>
+        /// <param name="controlName"> Uyarı mesajı sonrası focuslanılacak control adı </param>
+        public ZorunluAlan(string description, string controlName)
+        {
+            Description = description;
+            ControlName = controlName;
+        }
+    }
+}
